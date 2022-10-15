58dea2b1a09acff5f86fd1c29dfb2a9eb75cf1e1
jagiell0 <m.jagiello03@gmail.com>
2022-10-15 14:28:19 +0200

    Revert "Zmiany dodatkowe w 03"
    This reverts commit a85b526e0556651cf9bbd5b370185bfb3da73fdb.

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int policz(int a,int b)
 {
-    return (a-b)*(a+b);
+    return a*b;
 }
 int main() {
-    printf("W pierwszym branchu;2+3=%d!",policz(2,3));
+    printf("W pierwszym branchu;2*3=%d!",policz(2,3));
     return 0;
 }
